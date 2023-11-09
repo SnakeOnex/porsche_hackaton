73204dc355ac77fb4cc29c200353bd10a571d652
David Korčák <korcadav@fel.cvut.cz>
2023-11-09 10:04:58 +0100

    Better visualization for model, training code improvement

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import os
 from pathlib import Path
 from tqdm import tqdm
 from PIL import Image
+import argparse
 
 from torch.nn import functional as F
 from torch.utils.data import Dataset, DataLoader, Subset
@@ -11,6 +12,7 @@ from torchvision import transforms
 from torch.utils.data import random_split
 
 from utils import plot_sample
+
 
 class SteeringAngleDataset(Dataset):
     def __init__(self, transform=None):
@@ -44,22 +46,28 @@ class SteeringAngleDataset(Dataset):
         # B. get steering angles
         self.angles_file_path = self.data_dir / 'SteerValues' / 'steer_values.txt'
         with open(self.angles_file_path, 'r') as file:
-            self.steering_angles = [float(angle) for angle in file.read().splitlines()]
+            self.steering_angles = [float(angle)
+                                    for angle in file.read().splitlines()]
 
         assert len(self.image_paths) == len(self.steering_angles)
-        print(f'Loaded Kaggle dataset: {len(self.image_paths)} images with {len(self.steering_angles)} steering angles')
+        print(
+            f'Loaded Kaggle dataset: {len(self.image_paths)} images with {len(self.steering_angles)} steering angles')
 
     def print_stats(self):
         steering_angles = np.array(self.steering_angles)
         print(f'Steering angle stats: mean={np.rad2deg(steering_angles.mean()):.4f}, std={np.rad2deg(steering_angles.std()):.4f}, min={np.rad2deg(steering_angles.min()):.4f}, max={np.rad2deg(steering_angles.max()):.4f}')
 
+
 class ConvBlock(torch.nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
         super(ConvBlock, self).__init__()
-        self.conv = torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
+        self.conv = torch.nn.Conv2d(
+            in_channels, out_channels, kernel_size, stride, padding)
         self.bn = torch.nn.BatchNorm2d(out_channels)
+
     def forward(self, x):
         return F.relu(self.bn(self.conv(x)))
+
 
 class ConvNet(torch.nn.Module):
     def __init__(self):
@@ -88,18 +96,18 @@ class ConvNet(torch.nn.Module):
         x = self.fc2(x)
         return x
 
-def train(model, train_dataloader, valid_dataloader, criterion, optimizer, num_epochs):
+
+def train(model, train_dataloader, valid_dataloader, criterion, optimizer, num_epochs, device="cpu"):
     best_loss = 1e10
     best_model = None
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
     for epoch in range(num_epochs):
         # Training phase
         model.train()  # Set the model to training mode
         train_loss = 0.0
         for batch in tqdm(train_dataloader, desc=f'Epoch {epoch+1}/{num_epochs} - Train'):
             inputs = batch['image'].to(device)
-            targets = batch['steering_angle'].to(device).view(-1, 1)  # Ensure the targets are the correct shape
+            # Ensure the targets are the correct shape
+            targets = batch['steering_angle'].to(device).view(-1, 1)
 
             # Zero the parameter gradients
             optimizer.zero_grad()
@@ -113,7 +121,7 @@ def train(model, train_dataloader, valid_dataloader, criterion, optimizer, num_e
             optimizer.step()
 
             train_loss += loss.item() * inputs.size(0)
-        
+
         train_loss = train_loss / len(train_dataloader.dataset)
 
         # Validation phase
@@ -141,17 +149,19 @@ def train(model, train_dataloader, valid_dataloader, criterion, optimizer, num_e
         print(f'Epoch {epoch+1}/{num_epochs}, Train: {train_loss:.4f}, Valid: {val_loss:.4f}, Deg: {np.rad2deg(np.sqrt(val_loss)):.1f}')
 
 
-if __name__ == "__main__":
+def main(args):
     # 0. set seed & parameters
     torch.manual_seed(0)
-    max_dataset_size = 10000
-    num_epochs = 5
-    batch_size = 16
-    learning_rate = 0.001
+    max_dataset_size = args.max_dataset_size
+    num_epochs = args.num_epochs
+    batch_size = args.batch_size
+    train_fraction = args.train_size / max_dataset_size
+    learning_rate = args.lr
 
     # 1. setting up dataset and dataloaders
     transform = transforms.Compose([
-        transforms.Resize((220, 220)),  # Resize the image to the size expected by the conv-net
+        # Resize the image to the size expected by the conv-net
+        transforms.Resize((220, 220)),
         transforms.ToTensor(),  # Convert the image to a PyTorch tensor
         # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # Normalize for pre-trained models
     ])
@@ -160,34 +170,52 @@ if __name__ == "__main__":
     dataset.print_stats()
     dataset = Subset(dataset, range(max_dataset_size))
 
-    ## Define the proportions or absolute numbers for train and validation sets.
-    train_size = int(0.8 * len(dataset))
-    validation_size = len(dataset) - train_size
+    # Define the proportions or absolute numbers for train and validation sets.
+    train_size = int(train_fraction * len(dataset))
 
-    ## Randomly split the dataset into training and validation datasets.
+    # Randomly split the dataset into training and validation datasets.
     # train_dataset, validation_dataset = random_split(dataset, [train_size, validation_size])
     train_dataset = Subset(dataset, range(train_size))
     validation_dataset = Subset(dataset, range(train_size, len(dataset)))
 
-    ## setting up dataloader
-    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    valid_dataloader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=False)
+    # setting up dataloader
+    train_dataloader = DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=True)
+    valid_dataloader = DataLoader(
+        validation_dataset, batch_size=batch_size, shuffle=False)
+
+    device = torch.device('cuda:0' if torch.cuda.is_available(
+    ) else "mps" if torch.backends.mps.is_available() else 'cpu')
 
     # 2. setting up model & training parameters
     model = ConvNet()
+    model.to(device)
     criterion = torch.nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-    # 3. training
-    train(model, train_dataloader, valid_dataloader, criterion, optimizer, num_epochs)
+    if not Path("best_model.pth").exists() or args.force_retrain:
+        # 3. training
+        train(model, train_dataloader, valid_dataloader,
+              criterion, optimizer, num_epochs, device=device)
 
-    # load the best model
-    state_dict = torch.load('best_model.pth')
+    state_dict = torch.load('best_model.pth', map_location=device)
     model.load_state_dict(state_dict)
-
     val_idx = 285
     real_idx = train_size + val_idx
     image = dataset[real_idx]['image']
-    pred = model(image.unsqueeze(0)).item()
+    image_tensor = image.to(device)
+    pred = model(image_tensor.unsqueeze(0)).detach().cpu().item()
     steering_angle = dataset[real_idx]['steering_angle']
     plot_sample(image, steering_angle, pred)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--max_dataset_size', type=int, default=10000)
+    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--train_size', type=int, default=8000)
+    parser.add_argument('--num_epochs', type=int, default=5)
+    parser.add_argument('--lr', type=float, default=0.001)
+    parser.add_argument('--force_retrain', action='store_true', default=False)
+    args = parser.parse_args()
+    main(args)
--- utils.py
+++ utils.py
@@ -1,12 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
 
-def plot_sample(image, steer_gt, steer_pred=None):
+
+def plot_sample(image, steer_gt, steer_pred=None, steering_arrow_length=40):
+    steering_wheel_pos = image.shape[1] // 2, image.shape[2]
     plt.figure(figsize=(8, 8))
     plt.imshow(image.permute(1, 2, 0))
+    plt.xlim(0, image.shape[1])
+    plt.ylim(image.shape[2], 0)
     title_str = f"Steer GT: {np.rad2deg(steer_gt):.2f}"
     if steer_pred is not None:
         title_str += f" Steer Pred: {np.rad2deg(steer_pred):.2f}"
+    plt.arrow(steering_wheel_pos[0], steering_wheel_pos[1], steering_arrow_length * np.sin(
+        steer_gt), -steering_arrow_length * np.cos(steer_gt), color='r', width=2, label='Steering angle ground truth')
+    plt.arrow(steering_wheel_pos[0], steering_wheel_pos[1], steering_arrow_length * np.sin(steer_pred), -steering_arrow_length * np.cos(
+        steer_pred), color='g', width=2, label='Steering angle prediction')
+    plt.legend()
     plt.title(title_str)
     plt.show()
-
